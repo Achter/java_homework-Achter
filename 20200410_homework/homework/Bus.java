afa4b30ad8ea06b068708193ca73b7102b177dbc
Achter <1309856503@qq.com>
2020-04-13 09:41:45 +0800

    update

---
+++ 20200410_homework/homework/Bus.java
@@ -0,0 +1,24 @@
+package com.Achter.homework;
+/**
+ * 
+ * @author Achter
+ * @date   2020年4月10日
+ *
+ */
+public class Bus {
+
+	public static void main(String[] args) {
+		  
+		  int num = 321;
+		  String run_time = "06:30-23:10";
+		  String seat_capa = "50人";
+		  String run_rout = "吴中>>阳澄湖>>大闸蟹";
+		  
+		  System.out.println("公交车");
+		  System.out.println("编号:"+num);
+		  System.out.println("运行时间:"+run_time);
+		  System.out.println("载客数量:"+seat_capa);
+		  System.out.println("运行路线:"+run_rout);
+	}
+
+}
